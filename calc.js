5909685ec147ce5b2f9f8ae8d54ff7e908e4c195
Brandon Brumfield <brandonbrumfield7@gmail.com>
2025-06-10 15:39:02 -0500

    Add keyboard support, add decimal btn, fix bugs

--- calc.js
+++ calc.js
@@ -7,6 +7,10 @@ let current_var = "x"
 let display_sim = true;
 let equals = false;
 
+let typing_x = true;
+let operator_display = true;
+let last_btn = 0; 
+
 // Applies given operation (op) to x and y then stores in z. 
 function operate(op, X, Y) { 
     
@@ -83,6 +87,8 @@ const divide_btn = document.querySelector("#divide");
 const equals_btn = document.querySelector("#equals"); 
 const clear_btn = document.querySelector("#clear"); 
 const delete_btn = document.querySelector("#delete"); 
+const button_container = document.querySelector("#button_container")
+const decimal_btn = document.querySelector("#decimal");
 
 const display = document.querySelector("#display")
 display.textContent = "0";
@@ -151,11 +157,40 @@ nine.addEventListener("click", () => {
 })
 
 add_btn.addEventListener("click", () => {
+    //Ensure + isn't being clicked multiple times in a row.
+    if (last_btn == "+") {
+        return;
+    }
+
+    else if (last_btn == "X" || last_btn == "-" || last_btn == "/") {
+        display.textContent = "+";
+        op = "add";
+    }
+
     
-    op = "add";
-    display.textContent = "+";
-    x = display_var;
-    display_var = "";
+    else if (typing_x == true) {
+        op = "add";
+        display.textContent = "+";
+        x = display_var;
+        display_var = "";
+        typing_x = false;
+        
+    }
+
+    else if (typing_x == false) {
+        equals = true;
+        y = display_var;
+        operate(op,parseFloat(x),parseFloat(y))
+        x = display_var;
+        op = "add"
+        
+    }
+
+
+
+    
+
+
 
 
 
@@ -165,36 +200,114 @@ add_btn.addEventListener("click", () => {
 })
 
 equals_btn.addEventListener("click", () => {
-    equals = true;
-    y = display_var;
-    operate(op,parseFloat(x),parseFloat(y))
+    
+    if (last_btn == "=") {
+        return;
+    }
+    
+    
+    else {
+        equals = true;
+        typing_x = true;
+        y = display_var;
+        operate(op,parseFloat(x),parseFloat(y))
+    }
 
 })
 
 
 subtract_btn.addEventListener("click", () => {
-    op = "subtract";
-    display.textContent = "-";
-    x = display_var;
-    display_var = "";
+    
+    if (last_btn == "-") {
+        return;
+    }
+
+    else if (last_btn == "+" || last_btn == "X" || last_btn == "/") {
+        display.textContent = "-";
+        op = "subtract";
+    }
+    
+    else if (typing_x == true) {
+        op = "subtract";
+        display.textContent = "-";
+        x = display_var;
+        display_var = "";
+        typing_x = false;
+        
+    }
+
+    else if (typing_x == false) {
+        equals = true;
+        y = display_var;
+        operate(op,parseFloat(x),parseFloat(y))
+        x = display_var;
+        op = "subtract"
+        
+    }
 
     
 })
 
 multiply_btn.addEventListener("click", () => {
-    op = "multiply";
-    display.textContent = "X";
-    x = display_var;
-    display_var = "";
+    
+    if (last_btn == "X") {
+        return;
+    }
+
+    else if (last_btn == "+" || last_btn == "-" || last_btn == "/") {
+        display.textContent = "X";
+        op = "multiply";
+    }
+    
+    else if (typing_x == true) {
+        op = "multiply";
+        display.textContent = "X";
+        x = display_var;
+        display_var = "";
+        typing_x = false;
+        
+    }
+
+    else if (typing_x == false) {
+        equals = true;
+        y = display_var;
+        operate(op,parseFloat(x),parseFloat(y))
+        x = display_var;
+        op = "multiply"
+        
+    }
 
     
 })
 
 divide_btn.addEventListener("click", () => {
-    op = "divide";
-    display.textContent = "/";
-    x = display_var;
-    display_var = "";
+    
+    if (last_btn == "/") {
+        return;
+    }
+
+    else if (last_btn == "+" || last_btn == "-" || last_btn == "X") {
+        display.textContent = "/";
+        op = "divide";
+    }
+
+    else if (typing_x == true) {
+        op = "divide";
+        display.textContent = "/";
+        x = display_var;
+        display_var = "";
+        typing_x = false;
+        
+    }
+
+    else if (typing_x == false) {
+        equals = true;
+        y = display_var;
+        operate(op,parseFloat(x),parseFloat(y))
+        x = display_var;
+        op = "divide"
+        
+    }
 
     
 })
@@ -208,5 +321,282 @@ clear_btn.addEventListener("click", () => {
     display_var = "";
     display.textContent = "0";
     time_for_y = false;
+    typing_x = true;
+})
+
+button_container.addEventListener("click", (event) => { 
+    
+    if (display_var.length > 7 ) {display_var = display_var.slice(0,7);}
+    
+    //Makes sure clicking something other than a button doesn't affect the last registered button click.
+    else if (event.target.textContent.slice(0,1) == "\n") {return;}
+    //Makes sure that operators can't be clicked multiple times in a row. 
+    else {last_btn = event.target.textContent}
+
+
+
+})
+
+document.addEventListener("keypress", (event) => { 
+    
+    if (display_var.length > 7 ) {display_var = display_var.slice(0,7);}
+    
+
+    else if (event.key == "0") {
+        if (equals == true) {display_var = ""; equals = false;}
+        display_var += "0";
+        display.textContent = display_var;
+    }
+
+    else if (event.key == "1") {
+        if (equals == true) {display_var = ""; equals = false;}
+        display_var += "1";
+        display.textContent = display_var;
+    }
+
+    else if (event.key == "2") {
+        if (equals == true) {display_var = ""; equals = false;}
+        display_var += "2";
+        display.textContent = display_var;
+    }
+
+    else if (event.key == "3") {
+        if (equals == true) {display_var = ""; equals = false;}
+        display_var += "3";
+        display.textContent = display_var;
+    }
+
+    else if (event.key == "4") {
+        if (equals == true) {display_var = ""; equals = false;}
+        display_var += "4";
+        display.textContent = display_var;
+    }
+
+    else if (event.key == "5") {
+        if (equals == true) {display_var = ""; equals = false;}
+        display_var += "5";
+        display.textContent = display_var;
+    }
+
+    else if (event.key == "6") {
+        if (equals == true) {display_var = ""; equals = false;}
+        display_var += "6";
+        display.textContent = display_var;
+    }
+
+    else if (event.key == "7") {
+        if (equals == true) {display_var = ""; equals = false;}
+        display_var += "7";
+        display.textContent = display_var;
+    }
+
+    else if (event.key == "8") {
+        if (equals == true) {display_var = ""; equals = false;}
+        display_var += "8";
+        display.textContent = display_var;
+    }
+
+    else if (event.key == "9") {
+        if (equals == true) {display_var = ""; equals = false;}
+        display_var += "9";
+        display.textContent = display_var;
+    }
+
+    else if (event.key == "+") {
+        
+        //Ensure + isn't being clicked multiple times in a row.
+        if (last_btn == "+") {
+            return;
+        }
+
+        else if (last_btn == "X" || last_btn == "-" || last_btn == "/") {
+            display.textContent = "+";
+            op = "add";
+            last_btn = "+";
+        }
+
+        
+        else if (typing_x == true) {
+            op = "add";
+            display.textContent = "+";
+            x = display_var;
+            display_var = "";
+            typing_x = false;
+            last_btn = "+";
+            
+        }
+
+        else if (typing_x == false) {
+            equals = true;
+            y = display_var;
+            operate(op,parseFloat(x),parseFloat(y))
+            x = display_var;
+            op = "add"
+            last_btn = "+";
+            
+        }
+
+    }
+
+    else if (event.key == "X") {
+        
+        if (last_btn == "X") {
+            return;
+        }
+
+        else if (last_btn == "+" || last_btn == "-" || last_btn == "/") {
+            display.textContent = "X";
+            op = "multiply";
+            last_btn = "X";
+        }
+        
+        else if (typing_x == true) {
+            op = "multiply";
+            display.textContent = "X";
+            x = display_var;
+            display_var = "";
+            typing_x = false;
+            last_btn = "X";
+            
+        }
+
+        else if (typing_x == false) {
+            equals = true;
+            y = display_var;
+            operate(op,parseFloat(x),parseFloat(y))
+            x = display_var;
+            op = "multiply"
+            last_btn = "X";
+            
+        }
+
+    }
+
+    else if (event.key == "/") {
+        
+        if (last_btn == "/") {
+            return;
+        }
+
+        else if (last_btn == "+" || last_btn == "-" || last_btn == "X") {
+            display.textContent = "/";
+            op = "divide";
+            last_btn = "/";
+        }
+
+        else if (typing_x == true) {
+            op = "divide";
+            display.textContent = "/";
+            x = display_var;
+            display_var = "";
+            typing_x = false;
+            last_btn = "/";
+            
+        }
+
+        else if (typing_x == false) {
+            equals = true;
+            y = display_var;
+            operate(op,parseFloat(x),parseFloat(y))
+            x = display_var;
+            op = "divide"
+            last_btn = "/";
+            
+        }
+
+    }
+
+    else if (event.key == "-") {
+        
+        if (last_btn == "-") {
+            return;
+        }
+
+        else if (last_btn == "+" || last_btn == "X" || last_btn == "/") {
+            display.textContent = "-";
+            op = "subtract";
+            last_btn = "-";
+        }
+        
+        else if (typing_x == true) {
+            op = "subtract";
+            display.textContent = "-";
+            x = display_var;
+            display_var = "";
+            typing_x = false;
+            last_btn = "-";
+            
+        }
+
+        else if (typing_x == false) {
+            equals = true;
+            y = display_var;
+            operate(op,parseFloat(x),parseFloat(y))
+            x = display_var;
+            op = "subtract"
+            last_btn = "-";
+            
+        }
+
+    }
+
+    else if (event.key == ".") {
+        if (equals == true) {display_var = ""; equals = false;}
+        display_var += ".";
+        display.textContent = display_var;
+    }
+
+
+    else if (event.key == "=") {
+        if (last_btn == "=") {
+        return;
+        }
+    
+    
+        else {
+            equals = true;
+            typing_x = true;
+            y = display_var;
+            operate(op,parseFloat(x),parseFloat(y))
+            last_btn = "=";
+        }   
+    }
+    
+    
+
+
+
+
+
+})
+
+document.addEventListener("keydown", (event) => { 
+    if (event.key == "Backspace") {
+        if (last_btn == "+" || last_btn == "-" || last_btn == "X" || last_btn == "/") {
+        return;
+        }
+        else {
+            display_var = display_var.slice(0,-1);
+            display.textContent = display_var;
+        }
+    }
+})
+
+
+
+delete_btn.addEventListener("click", () => {
+    if (last_btn == "+" || last_btn == "-" || last_btn == "X" || last_btn == "/") {
+        return;
+    }
+    else {
+        display_var = display_var.slice(0,-1);
+        display.textContent = display_var;
+    }
+})
+
+decimal_btn.addEventListener("click", () => {
+    if (equals == true) {display_var = ""; equals = false;}
+    display_var += ".";
+    display.textContent = display_var;
 })
 
